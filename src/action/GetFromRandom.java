2bf96a1261e785035459277d2279ac86bdc74352
Demidova <demidova.ma@yandex.ru>
2024-12-21 19:13:00 +0500

    консольUI (не полное)

---
+++ src/action/GetFromRandom.java
@@ -0,0 +1,12 @@
+package action;
+
+import java.util.List;
+
+public class GetFromRandom implements IAction{
+
+
+    @Override
+    public String act(String choise, List arr) {
+        return "";
+    }
+}
---
+++ src/action/UserChoice.java
@@ -0,0 +1,5 @@
+package action;
+
+public enum UserChoice {
+DATA_FROM_FILE, RANDOM_DATA, DATA_FROM_USER
+}
